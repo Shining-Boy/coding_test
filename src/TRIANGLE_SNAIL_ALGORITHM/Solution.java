32acc8540904f57315cf63d0b3996693cf488b83
hbae <hsbae@anchors-biz.com>
2023-04-15 05:56:56 +0900

    Create Solution Class for Triangle Snail

---
+++ src/TRIANGLE_SNAIL_ALGORITHM/Solution.java
@@ -0,0 +1,12 @@
+package TRIANGLE_SNAIL_ALGORITHM;
+
+public class Solution {
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+
+    public int[] solution(int n){
+        int[] result = new int[10];
+
+        return result;
+    }
+}
